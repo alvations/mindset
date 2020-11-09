4a3c3ee36b93c1e2f981e74460bfdd02f75ac3d9
alvations <alvations@gmail.com>
2020-11-09 15:41:58 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+import setuptools
+
+setup(
+  name = 'mindset',
+  packages = ['mindset'],
+  version = '0.0.1',
+  description = 'Mindset',
+  author = '',
+  url = 'https://github.com/alvations/mindset',
+  keywords = [],
+  classifiers = [
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+  ],
+)
